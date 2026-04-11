2ef8693fd4f12d806cddebd63cd7de6fc9c50d39
erinecassidy <erin.cassidy1209@gmail.com>
2026-04-11 15:00:06 -0600

    yikes

--- JS_CSS_downloaded_libraries/JSDownloaded.js
+++ JS_CSS_downloaded_libraries/JSDownloaded.js
@@ -1,4 +1,4 @@
 <head>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
-    </head>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
